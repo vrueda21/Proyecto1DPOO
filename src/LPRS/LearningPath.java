17e62c05de99f55fdb62c152e6cdf0468ba98823
vrueda21 <v.ruedac@uniandes.edu.co>
2024-10-28 00:57:16 -0500

    Avance Estudiante y Comienzo LearningPath

--- src/LPRS/LearningPath.java
+++ src/LPRS/LearningPath.java
@@ -1,5 +1,25 @@
 package LPRS;
+import actividad.*;
+
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class LearningPath {
 
+    protected String titulo;
+    protected Nivel nivelDificultad;
+    protected String descripcion;
+    protected List<String> objetivos;
+    protected int duracionMinutos;
+    protected LocalDateTime fechaCreacion;
+    protected LocalDateTime fechaModificacion;
+    protected double version;
+    protected Status status;
+
+
+    public LearningPath(String titulo, Nivel nivelDificultad, String descripcion, int duracionMinutos, Status status){
+
+        
+
+    }
 }
--- src/usuario/Estudiante.java
+++ src/usuario/Estudiante.java
@@ -1,18 +1,47 @@
 package usuario;
 import plataforma.*;
 import actividad.*;
+import LPRS.LearningPath;
+import java.util.*;
 
 public class Estudiante extends Usuario{
+
+    protected Actividad actividadActual;
+    protected LearningPath learningPathActual;
+    protected List<LearningPath> listaLearningPathsCompletados;
+    protected List<Actividad> listaActividadesCompletadas;
 
     public Estudiante(String nombre, String contrasenia, String correo){
 
         super(nombre, contrasenia, "estudiante", correo);
+        this.actividadActual=null;
+        this.learningPathActual=null;
+        this.listaActividadesCompletadas = new ArrayList<>();
     }
 
     public void prueba(){
         Administrador administrador = Administrador.getAdmin(); //LLamar a administrador
 
+    }
+    public Actividad getActividadActual() {
+        return actividadActual;
+    }
 
+    public void setActividadActual(Actividad actividadActual) {
+        this.actividadActual = actividadActual;
+    }
+
+    public List<Actividad> getListaActividadesCompletadas() {
+        return listaActividadesCompletadas;
+    }
+    
+
+    public LearningPath getLearningPathActual() {
+        return learningPathActual;
+    }
+
+    public void setLearningPathActual(LearningPath learningPathActual) {
+        this.learningPathActual = learningPathActual;
     }
 
     public static boolean registrarEstudiante(String nombre, String correo, String contrasenia) {
@@ -25,6 +54,25 @@ public class Estudiante extends Usuario{
     public void marcarCompletada(Tarea tarea){
 
         tarea.marcarCompletada(this);
+        listaActividadesCompletadas.add(this.actividadActual);
+        this.actividadActual=null;
+
+    }
+
+    public Actividad comenzarActividad(Actividad actividad) throws IllegalStateException{
+        if (actividadActual==null){
+            setActividadActual(actividad);
+            return actividadActual;
+        }
+
+        else{
+            throw new IllegalStateException("No se puede comenzar una actividad nueva porque hay una en progreso.");
+        }
+        
+    }
+
+    public void agregarLearningPathCompletado(LearningPath learningPath){
+
 
     }
     
